80484c61d1f45b7f95112879b42bd5e559b0275f
Osama Yousuf <osamayousuf26@gmail.com>
2024-01-31 18:41:21 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -5,67 +5,64 @@ import {
   useMutation,
   useQueryClient,
   QueryClient,
-  QueryClientProvider,
-} from 'react-query'
-import { checkUser, createTodo, getAllTodo } from './utils';
+} from '@tanstack/react-query'
+// import { checkUser, createTodo, getAllTodo } from './utils';
 import { useState } from 'react';
 
 function App() {
-  const queryClient = useQueryClient()
   const [title, setTitle] = useState("")
   const [search, setSearch] = useState('')
-
-  // Queries Data List
-  const { data, isLoading } = useQuery(
+  const queryClint = useQueryClient()
+  // Queries for getting data
+  const { data } = useQuery(
     {
-      queryFn: () => getAllTodo(search),
-      queryKey: ['todos', search],
+      queryKey: ['advertisement'], //just like useEffect Dependency
+      // queryFn: async () => { await GetAllAdvertisement({ type: "All" }) },
     })
 
-  // Mutations All
-  const createMutation = useMutation(createTodo, {
-    onSuccess: () => {
+  // Mutations for creating data
+  const { mutateAsync: handleMe } = useMutation({
+    // mutationFn: async (body) => await CreateAdvertisement(body),
+    onSuccess: (data) => {
+      console.log("api data", data)
       // Invalidate and refetch
-      queryClient.invalidateQueries('todos')
+      queryClint.invalidateQueries('advertisement')
       alert("Succesfully Created")
     },
   })
 
-  const checkMutation = useMutation(checkUser, {
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries('todos')
-    },
-  })
+  // create advertisement
+  const handleCreateAd = async (value) => {
+    const body = {
+      kioskId: ""
+    }
+    await handleMe(body)
+    // try {
+    //     if (type === "0" && !img) return
+    //     if (type === "1" && !video) return
+    //     setLoader(true)
+    //     await CreateAdvertisement(body)
+    //     toast.success("Advertisement successfully created")
+    //     setLoader(false)
+    // } catch (err) {
+    //     toast.error("Some error occured")
+    //     setLoader(false)
 
-  // handle add 
-  const handleAdd = () => {
-    createMutation.mutate({
-      id: Date.now(),
-      name: title,
-    })
-    setTitle("")
+    // }
   }
 
-  // handle check 
-  const handleCheck = (item, index) => {
-    checkMutation.mutate({
-      ...item,
-      index: index,
-    })
-  }
 
   return (
     <div className="App">
       <input placeholder='Search User' onChange={(e) => setSearch(e?.target?.value)} value={search} type="text" />
       <input placeholder='Add User' onChange={(e) => setTitle(e?.target?.value)} value={title} type="text" />
-      <button onClick={handleAdd}>Add</button>
-      {isLoading && <h1>Loading</h1>}
-      {data?.map((item, index) => {
+      {/* <button onClick={handleAdd}>Add</button> */}
+      {/* {isLoading && <h1>Loading</h1>} */}
+      {/* {data?.map((item, index) => {
         return (
           <p> <input onChange={() => handleCheck(item, index)} checked={item?.check} type="checkbox" /> {item?.name}</p>
         )
-      })}
+      })} */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
